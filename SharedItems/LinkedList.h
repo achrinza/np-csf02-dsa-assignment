b1adf8c5c6bb3111cb057990bb2a15f4c177f36b
Rifa Achrinza <25147899+achrinza@users.noreply.github.com>
2021-01-25 17:45:56 +0800

    feat: add matching engine

---
+++ SharedItems/LinkedList.h
@@ -0,0 +1,7 @@
+#pragma once
+class LinkedList
+{
+private:
+
+};
+
---
+++ SharedItems/RiderDriverMatchingEngine.h
@@ -0,0 +1,14 @@
+#pragma once
+class RiderDriverMatchingEngine
+{
+private:
+	
+
+public:
+	int addDriver();
+	int addRider();
+	int removeDriver();
+	int removeRider();
+	int match();
+};
+

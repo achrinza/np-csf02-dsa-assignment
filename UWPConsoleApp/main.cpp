ba4d56ada94d7905b28b552b77d3e45279beaeec
Rifa Achrinza <25147899+achrinza@users.noreply.github.com>
2021-01-25 17:33:19 +0800

    feat: use winrt for console app

---
+++ UWPConsoleApp/main.cpp
@@ -0,0 +1,11 @@
+﻿#include "pch.h"
+
+using namespace winrt;
+using namespace Windows::Foundation;
+
+int main()
+{
+    winrt::init_apartment();
+
+    std::cout << "Hello, world!" << std::endl;
+}
---
+++ UWPConsoleApp/pch.h
@@ -0,0 +1,4 @@
+﻿#pragma once
+#include <iostream>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>

cd2e84dd1216c6e791f8441b8f966945b04df816
Jun Hao Lim <s10195387@connect.np.edu.sg>
2021-02-05 14:17:16 +0800

    feat/added_driver_class_temp

--- UWPConsoleApp/Driver.cpp
+++ UWPConsoleApp/Driver.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "Driver.h"
+#include "DriverOptions.h"
 #include <string>
 using namespace std;
 
@@ -11,26 +12,55 @@ Driver::Driver(string name, string carmodel, string plateno, int maxpax) {
 	Options = NULL;
 	Maxpax = maxpax;
 }
-void Driver::updateoptions(string* list) {
-	Options = list;
-}
-void Driver::addoption(string option) {
-	Options->append(option);
-}
-bool Driver::rmoption(string option) {
-	int index = -1;
-	for (int i = 0; i < Options->length(); i++) {
-		if (Options[i] == option)
-			index = i;
-	}
-	if (index == -1)
-		return false;
-	else {
-		for (int r = index; r < Options->length() - 1; r++) {
-			Options[r] = Options[r + 1];
-		}
+bool Driver::addoption(string op) {
+	DriverOptions* temp = new DriverOptions;
+	temp->option = op;
+	temp->next = NULL;
+	if (Options == NULL) {
+		Options = temp;
 		return true;
 	}
+	else {
+		DriverOptions* travel = new DriverOptions;
+		travel = Options;
+		while (travel != NULL && travel->option != op) {
+			if (travel->next == NULL) {
+				travel->next = temp;
+				return true;
+			}
+			travel = travel->next;
+		}
+		return false;
+	}
+}
+void Driver::rmoption(string op) {
+	if (Options != NULL) {
+		DriverOptions* del = new DriverOptions;
+		del = Options;
+		while (del != NULL && del->option != op)
+			del = del->next;
+		if (del != NULL) {
+			DriverOptions* cut = new DriverOptions;
+			while (cut->next != del)
+				cut = cut->next;
+			cut->next = del->next;
+			delete del;
+			del = NULL;
+		}
+	}
+}
+void Driver::viewoptions() {
+	if (Options == NULL)
+		cout << "No options" << endl;
+	else {
+		DriverOptions* display = new DriverOptions;
+		display = Options;
+		while (display != NULL) {
+			cout << display->option << ", ";
+			display = display->next;
+		}
+	}
+		
 }
 void Driver::updatecar(string carmodel, string plateno, int maxpax) {
 	Carmodel = carmodel;
--- UWPConsoleApp/Driver.h
+++ UWPConsoleApp/Driver.h
@@ -1,23 +1,25 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include "DriverOptions.h"
 using namespace std;
 
 class Driver
 {
+
 private:
 	string Fullname;
 	string Carmodel;
 	int Maxpax;
-	string* Options;
+	DriverOptions* Options;
 	string Plateno;
 
 public:
 	Driver();
 	Driver(string name, string carmodel, string plateno,int maxpax);
-	void updateoptions(string* listofoptions);
-	void addoption(string option);
-	bool rmoption(string option);
+	bool addoption(string op);
+	void rmoption(string op);
+	void viewoptions();
 	void updatecar(string carmodel, string plateno, int maxpax);
 
 };

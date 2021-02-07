10e067ad6e7322c236994c551a20d8bac8a85ee8
Jun Hao Lim <s10195387@connect.np.edu.sg>
2021-02-08 00:04:29 +0800

    feat/added comments to JH represented classes

--- UWPConsoleApp/location.h
+++ UWPConsoleApp/location.h
@@ -1,5 +1,6 @@
 #pragma once
-
+//Team:12 - Member:Jun Hao - Student ID:S10195387//
+//This is the location class. It stores the coordinates of a location data point//
 class Location
 {
 private:
@@ -9,6 +10,14 @@ private:
 public:
 	Location();
 	Location(double x, double y);
+
+	//Retrieves the coordinates of an instance of this class//
+	//Input parameters: None//
+	//Output: Returns a pointer to an array of {x, y}//
 	double* getCoordinate();
+
+	//Computes the sum of x and y coordinates of an instance of this class//
+	//Input parameters: None//
+	//Output: Returns a double of the sum of x and y coordinates//
 	double getSum();
 };
